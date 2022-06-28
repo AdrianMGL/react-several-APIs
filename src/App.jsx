f6d30057012b5052d78a6fa1da85dad8b778e23a
Adrian MGL <adrienguzmanlopez@gmail.com>
2022-06-28 18:12:20 -0500

    Repaso useState

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [counter, setCounter] = useState(0);
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    console.log("Se ejecutó COUNTER");
+
+    const colors = [
+      "#845EC2",
+      "#D65DB1",
+      "#FF6F91",
+      "#FF9671",
+      "#FFC75F",
+      "#F9F871",
+    ];
+
+    const randomColor = Math.floor(Math.random() * colors.length); // 5
+
+    document.body.style = `background: ${colors[randomColor]}`;
+  }, [isVisible]);
+
+  return (
+    <div className="App">
+      <h1>Counter</h1>
+      <h3>{counter}</h3>
+      <button onClick={() => setCounter(counter + 1)}>Increment</button>
+      <button onClick={() => setCounter(counter - 1)}>Decrement</button>
+      <hr />
+      <h1>Input contraseña</h1>
+      <input type={isVisible ? "text" : "password"} />
+      <button onClick={() => setIsVisible(!isVisible)}>Show/hidde</button>
+    </div>
+  );
+}
+
+export default App;
